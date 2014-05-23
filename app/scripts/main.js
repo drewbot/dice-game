3a20f675f259f53c4bb14a1c7e822fafca2fcabc
Drew Botka <hellodrewbot@gmail.com>
2014-05-23 10:57:00 -0400

    Basic JS functionality and gruntfile update.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+
+
+//Store a function as a variable which will generate a number between one and x
+//x to be specified in future use of the function
+var rollDice = function(x) {
+  var randomNumber = Math.floor(Math.random() * x + 1);
+  $('.face-single').text(randomNumber);
+  console.log(randomNumber);
+}
+
+//When this button is clicked a random number will be generated and appended to the face-single div
+$('.button-single').click(function(){
+  rollDice(100);
+});
+
+
+var rollDiceDouble = function(x) {
+  var randomNumber = Math.floor(Math.random() * x + 1);
+  $('.face-double1').text(randomNumber);
+  console.log(randomNumber);
+}
+
+
+var rollDiceDouble2 = function(x) {
+  var randomNumber = Math.floor(Math.random() * x + 1);
+  $('.face-double2').text(randomNumber);
+  console.log(randomNumber);
+}
+
+$('.button-double').click(function(){
+  rollDiceDouble(6);
+  rollDiceDouble2(6);
+});
