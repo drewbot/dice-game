b3f2c2ab4fdeeaa9c791e901c90d73c6a56c8dc2
Drew Botka <hellodrewbot@gmail.com>
2014-05-23 14:31:29 -0400

    Added user input to dice sides.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,6 +13,7 @@ $('.button-single').click(function(){
   rollDice(100);
 });
 
+// Two at a time
 
 var rollDiceDouble = function(x) {
   var randomNumber = Math.floor(Math.random() * x + 1);
@@ -31,3 +32,54 @@ $('.button-double').click(function(){
   rollDiceDouble(6);
   rollDiceDouble2(6);
 });
+
+
+//Choose number of sides
+
+function Die (sides) {
+  this.sides = parseInt(sides);
+ 
+  this.roll = function(){
+    var result = Math.floor(Math.random() * this.sides + 1 );
+    console.log('rolled 1d'+ this.sides, " for ", result);
+    return result;
+  }
+}
+ 
+$('.create-die').click(function(){
+  var sides = $('.die-sides').val();
+  gameDie = new Die(sides);
+})
+
+$('.button-any').click(function(){
+  $('.face-any').text(gameDie.roll());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,12 +3,65 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
+    // describe('Give it some context', function () {
+    //     describe('maybe a bit more context here', function () {
+    //         it('should run here few assertions', function () {
+    //           var answer = 2;
+    //           expect(2).to.equal(answer);
+    //         });
+    //     });
+    // });
+
+
+    
+    describe("Create Die Button", function(){
+ 
+      // set input vaue to 7
+      $('.die-sides').val('7');
+      // then click the button
+      $('.create-die').click();
+ 
+      it("should create a die and store it in the gameDie global", function(){        
+        expect(gameDie.sides).to.equal(7);
+      });
+ 
+      it("should create a die with sides equal the input value", function(){
+        var inputVal = parseInt($('.die-sides').val())
+        expect(gameDie.sides).to.equal(inputVal);
+      });
+ 
     });
+ 
+    describe("Die Constructor", function(){
+ 
+      it("should impart a sides property to instances", function(){
+        var die = new Die(900);
+        expect(die).to.have.property('sides');
+      });
+ 
+      it("should accept a number or a number as a string as its argument", function(){
+        var die = new Die('7');
+        expect(die.sides).to.equal(7);
+      })
+ 
+      describe("imparts a roll function", function() {
+        it("that returns between its min and max values", function(){
+          var die = new Die('7');
+ 
+          for (var i = 0; i < 1000; i += 1) {
+            expect(die.roll()).to.be.within(1,7);
+          }
+        })
+      })
+ 
+    })
+
+
+
+
+
+
+
+
+    
 })();
